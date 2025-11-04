ccd1c72f6d8985abce28afe5724ec596ca22a492
Kashinath Kambar <kambarkashinath19@gmail.com>
2025-11-04 17:03:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,8 +16,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false
-  },
-  define: {
-    'process.env': {}
   }
 })
